889cd124a4c99bba63f1b4f8028c37896adb1bb9
IhorShvydkyi <igorshvydky35@gmail.com>
2022-01-19 22:21:13 +0200

    Navigation, first try routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./Navigation/Navigation";
 
 function App() {
-  return <div className="App">routes</div>;
+  return (
+    <div className="App">
+      <Navigation />
+    </div>
+  );
 }
-
 export default App;
